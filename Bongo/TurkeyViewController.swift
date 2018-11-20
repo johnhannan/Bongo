4192bdcc8767c528e26eef39cc23bda04ed2cdd6
John Hannan <hannan@cse.psu.edu>
2018-11-20 09:04:54 -0500

    for class

--- Bongo/TurkeyViewController.swift
+++ Bongo/TurkeyViewController.swift
@@ -15,6 +15,7 @@ class TurkeyViewController: UIViewController, UICollisionBehaviorDelegate {
     let motionManager = CMMotionManager()
     
     var dynamicAnimator : UIDynamicAnimator!
+    var gravityAnimator : UIDynamicAnimator!
     var turkeyGravityBehavior : UIGravityBehavior!
     var gravityBehavior : UIGravityBehavior!
     var collisionBehavior : UICollisionBehavior!
@@ -23,6 +24,7 @@ class TurkeyViewController: UIViewController, UICollisionBehaviorDelegate {
     let motionQueue = OperationQueue()
     
     let turkeyView = UIImageView(image: UIImage(named: "turkey"))
+    var items = [UIImageView]()
     var timer : Timer?
     
     override func viewDidLoad() {
@@ -44,7 +46,9 @@ class TurkeyViewController: UIViewController, UICollisionBehaviorDelegate {
                 let y = acceleration.y
 
                 DispatchQueue.main.async {
+                    if self.turkeyGravityBehavior != nil {
                     self.turkeyGravityBehavior.gravityDirection = CGVector(dx: scale*x, dy: -scale*y)
+                    }
                 }
                 
             }
@@ -84,19 +88,20 @@ class TurkeyViewController: UIViewController, UICollisionBehaviorDelegate {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         turkeyView.center = self.view.center
-       
+        if dynamicAnimator == nil {
+            addBehaviors()
+        }
     }
     
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        if dynamicAnimator != nil {
-            addBehaviors()
-        }
+
     }
 
     func addBehaviors() {
         dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        gravityAnimator = UIDynamicAnimator(referenceView: self.view)
         
         collisionBehavior = UICollisionBehavior(items: [turkeyView])
         collisionBehavior.translatesReferenceBoundsIntoBoundary = true
@@ -109,7 +114,7 @@ class TurkeyViewController: UIViewController, UICollisionBehaviorDelegate {
         itemBehaviors.density = 0.0
         
         gravityBehavior = UIGravityBehavior()
-        gravityBehavior.gravityDirection = CGVector(dx: 0.0, dy: -10.0)
+        gravityBehavior.gravityDirection = CGVector(dx: 0.0, dy: 3.0)
         
         turkeyGravityBehavior = UIGravityBehavior(items: [turkeyView])
         turkeyGravityBehavior.gravityDirection = CGVector.zero
@@ -119,7 +124,10 @@ class TurkeyViewController: UIViewController, UICollisionBehaviorDelegate {
         dynamicAnimator.addBehavior(collisionBehavior)
         dynamicAnimator.addBehavior(itemBehaviors)
         dynamicAnimator.addBehavior(turkeyGravityBehavior)
-        dynamicAnimator.addBehavior(gravityBehavior )
+        gravityAnimator.addBehavior(gravityBehavior )
+        
+        collisionBehavior.action = {}
+        
         
         }
 
@@ -144,9 +152,20 @@ class TurkeyViewController: UIViewController, UICollisionBehaviorDelegate {
             axeView.frame = CGRect(origin: origin, size: axeView.bounds.size)
             self.view.addSubview(axeView)
             gravityBehavior.addItem(axeView)
+            collisionBehavior.addItem(axeView)
         }
     }
 
+    func collisionBehavior(_ behavior: UICollisionBehavior, endedContactFor item1: UIDynamicItem, with item2: UIDynamicItem) {
+        
+        
+    }
+    
+    func endGame() {
+        togglePlay()
+        let alertView = UIAlertController(title: "Game Over", message: "", preferredStyle: .alert)
+        present(alertView, animated: true, completion: nil)
+    }
 }
 
 
